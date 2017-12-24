7cbdaba05fc144061d9d7b57b142787d22e2058b
Andrew Meiling <ameiling@circadence.com>
2017-12-24 12:37:14 -0800

    A simple app to learn TDD/BDD with Jest

---
+++ src/components/App.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from './App';
+
+configure({ adapter: new Adapter() });
+
+describe('App', () => {
+    const app = shallow(<App />);
+
+    it('renders correctly', () => {
+       expect(app).toMatchSnapshot(); 
+    });
+    
+    it('initializes the `state` with an empty list of gifts', () => {
+        expect(app.state().gifts).toEqual([]);
+    })
+
+    describe('When clicking the `add-gift` button', () => {
+        const id = 1;
+
+        beforeEach(() => {
+            app.find('.btn-add').simulate('click');
+        });
+
+        afterEach(() => {
+            app.setState({ gifts: [] });
+        })
+
+        it('adds a new gift to `state`', () => {
+            expect(app.state().gifts).toEqual([{ id }]);
+        });
+        
+        it('adds a new gift to the rendered list', () => {
+            expect(app.find('.gift-list').children().length).toEqual(1);
+        });
+
+        it('creates a Gift component', () => {
+            expect(app.find('Gift').exists()).toBe(true);
+        });
+    });
+
+    describe('and the user wants to remove the added gift', () => {
+        const id = 1;
+
+        beforeEach(() => {
+            app.instance().removeGift(id);
+        });
+
+        it('removes the gift from `state`', () => {
+            expect(app.state().gifts).toEqual([]);
+        });
+    });
+});
+
